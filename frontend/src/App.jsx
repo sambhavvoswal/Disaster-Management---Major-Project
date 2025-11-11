8d378c1434bbbd8cdb11fbbe371c23b9939b5109
sambhav oswal <sambhavoswal23@gmail.com>
2025-11-11 15:30:38 +0530

    Added interractive dock to home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,6 +10,8 @@ import Home from './components/Home'
 import Header from './components/Home/Header'
 import Footer from './components/Home/Footer'
 import Interface from './components/Newhome/Interface'
+import Dock from './components/General/Dock'
+import { HomeIcon, Squares2X2Icon, SunIcon } from '@heroicons/react/24/outline'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -20,12 +22,34 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Interface />} />
-          {/* <Route path="/" element={<Home />} /> */}
+          <Route path="/Home" element={<Home />} />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/general-weather" element={<GeneralWeather />} />
         </Routes>
       </BrowserRouter>
       <Footer />
+      <Dock 
+        items={[
+          {
+            icon: <HomeIcon size={18} />,
+            label: 'Home',
+            onClick: () => (window.location.href = '/')
+          },
+          {
+            icon: <Squares2X2Icon size={18} />,
+            label: 'Dashboard',
+            onClick: () => (window.location.href = '/dashboard')
+          },
+          {
+            icon: <SunIcon size={18} />,
+            label: 'General Weather',
+            onClick: () => (window.location.href = '/general-weather')
+          },
+        ]}
+        panelHeight={65}
+        baseItemSize={40}
+        magnification={60}
+      />
     </>
   )
 }
