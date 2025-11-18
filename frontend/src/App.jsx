698546893e40e56360225ba66642f11db00116a2
sambhav oswal <sambhavoswal23@gmail.com>
2025-11-18 22:51:14 +0530

    Enabled login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -23,8 +23,8 @@ function App() {
     <Header />
       <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
         <Routes>
-          <Route path="/" element={<Interface />} />
-          {/* <Route path="/" element={<Login />} /> */}
+          {/* <Route path="/" element={<Interface />} /> */}
+          <Route path="/" element={<Login />} />
           <Route path="/Home" element={<Interface />} />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/air-quality" element={<GeneralWeather />} />
